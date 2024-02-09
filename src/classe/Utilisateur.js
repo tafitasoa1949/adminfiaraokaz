8e4e0e36e2b7c45c3b58b2fa0fad3e355fe4f5cc
Tafitasoa RATSIMBAHARISON <118884621+tafitasoa1949@users.noreply.github.com>
2024-02-09 21:02:45 +0300

    sprint final

--- src/classe/Utilisateur.js
+++
@@ -1,7 +0,0 @@
-class Utilisateur{
-    constructor( pseudo,password) {
-       this.pseudo=pseudo;
-       this.password=password;
-    }
-}
-export default Utilisateur;
--- src/page/annonce/valider/class/Favorie.js
+++
@@ -1,9 +0,0 @@
-class Favorie {
-    constructor(idfavorie,idclient, idfcaracteristique) {
-        this.idfavorie=idfavorie;
-        this.idclient = idclient;
-        this.idfcaracteristique = idfcaracteristique;
-    }
-  
-}
-export default Favorie;
